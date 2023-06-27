afc24df5cd4ab22397b45f07b89783c899936c6f
Atsuhiro <atcheri@gmail.com>
2023-06-27 15:40:05 +0200

    feat: add 2 main functions to contract

--- src/contracts/Crodfunding.sol
+++ src/contracts/Crodfunding.sol
@@ -2,5 +2,36 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Crowdfunding {
-    
+    mapping(address => uint) public Contributors;
+    address public manager;
+    uint public minContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public numOfcontributors;
+
+    constructor(uint _target, uint _deadline) {
+        target = _target;
+        deadline = block.timestamp + _deadline;
+        minContribution = 100 wei;
+        manager = msg.sender;
+    }
+
+    function contribute() public payable {
+        require(block.timestamp < deadline, "the deadline has been passed");
+        require(
+            msg.value >= minContribution,
+            "the minimum contribution of 100 wei is not met"
+        );
+
+        if (Contributors[msg.sender] == 0) {
+            numOfcontributors++;
+        }
+        Contributors[msg.sender] = msg.value;
+        raisedAmount += msg.value;
+    }
+
+    function balance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
